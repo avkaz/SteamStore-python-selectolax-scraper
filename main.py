1abf6f9013c8f493959df62a139d75c6e5d46e36
avkaz <av.kazakov55@gmail.com>
2023-10-27 12:19:29 +0200

    added content to tools.py file. Added parse.py, which parse data from config to main. Added process.py, which transforms and modifies data while parsing from config to main.

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 from selectolax.parser import HTMLParser
 from utils.extract import extract_full_body_html
-
-url = 'https://store.steampowered.com/specials'
-wait_for_tag = 'div[class*="salepreviewwidgets_StoreSaleWidgetContainer"]'
+from utils.parse import parse_raw_attributes
+from config.tools import get_config
+import pandas as pd
 
 if __name__ == '__main__':
+    config = get_config()
+    html = extract_full_body_html(
+        from_url = config.get('url'), 
+        wait_for = config.get('container').get('selector')
+        )
+    
 
-    html = extract_full_body_html(from_url = url, wait_for = wait_for_tag)
     tree = HTMLParser(html)
     divs = tree.css('div[class*="salepreviewwidgets_StoreSaleWidgetContainer"]') # *= is should contain
 
-    print(len(divs))
-
     for d in divs:
-        title = d.css_first('div[class*="salepreviewwidgets_StoreSaleWidgetTitle"]').text()
-        thumbnail = d.css_first('img[class*="salepreviewwidgets_CapsuleImage"]').attributes.get('src')
-        tags = [a.text() for a in d.css('div[class*="salepreviewwidgets_StoreSaleWidgetTags"] > a')[:5]]
-        release_date = d.css_first('div[class*="salepreviewwidgets_WidgetReleaseDateAndPlatformCtn"] > div[class*= "salepreviewwidgets_StoreSaleWidgetRelease"]').text()
-        rate = d.css_first('div[class*="ReviewScoreValue"] div').text()
-        reviewed_by = d.css_first('div[class*="ReviewScoreCount"]').text()
-        original_price = d.css_first('div[class*="StoreOriginalPrice"]').text()
-        sale_price = d.css_first('div[class*="StoreSalePriceBox"]').text()
-        attrs = {'title': title,
-                    'release_date': release_date,
-                    'rate': rate,
-                    'reviewed_by': reviewed_by,
-                    'original_price': original_price,
-                    'sale_price': sale_price,
-                    'tags': tags,
-                    'thumbnail' : thumbnail,
-                    }
+        attrs = parse_raw_attributes(d, config.get('item'))
+        # title = d.css_first('div[class*="StoreSaleWidgetTitle"]').text()
+        # thumbnail = d.css_first('img[class*="CapsuleImage"]').attributes.get('src')
+        # tags = [a.text() for a in d.css('div[class*="StoreSaleWidgetTags"] > a')[:5]]
+        # release_date = d.css_first('div[class*="WidgetReleaseDateAndPlatformCtn"] > div[class*= "StoreSaleWidgetRelease"]').text()
+        # review_score = d.css_first('div[class*="ReviewScoreValue"] div').text()
+        # reviewed_by = d.css_first('div[class*="ReviewScoreCount"]').text()
+        # original_price_element = d.css_first('div[class*="StoreOriginalPrice"]')
+        # original_price = original_price_element.text() if original_price_element else ""
+        # # original_price = d.css_first('div[class*="StoreOriginalPrice"]').text()            
+        # sale_price = d.css_first('div[class*="StoreSalePriceBox"]').text()
+        # attrs = {'title': title,
+        #             'release_date': release_date,
+        #             'review_score': review_score,
+        #             'reviewed_by': reviewed_by,
+        #             'original_price': original_price,
+        #             'sale_price': sale_price,
+        #             'tags': tags,
+        #             'thumbnail' : thumbnail,
+        #             }
         print(attrs)
+
+    df = pd.DataFrame(attrs, columns=["title", "thumbnail", "release_date", "review_score", "reviewed_by", "price_currency", "sale_price", "original_price"])
+    df.set_index("title", inplace=True)
+
+    print(df)
--- utils/extract.py
+++ utils/extract.py
@@ -1,18 +1,15 @@
 from playwright.sync_api import sync_playwright
 
-url = 'https://store.steampowered.com/specials'
-wait_for = 'div[class*="salepreviewwidgets_StoreSaleWidgetContainer"]'
-
 def extract_full_body_html(from_url, wait_for = None):
     with sync_playwright() as p:
-        TIMEOUT = 9000
+        TIMEOUT = 30000
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(from_url)
         page.wait_for_load_state("networkidle", timeout=TIMEOUT)
         page.evaluate("() => window.scroll(0, document.body.scrollHeight)")
         # page.wait_for_load_state('load', timeout=TIMEOUT)
-        page.wait_for_timeout(1000)
+        page.wait_for_timeout(3000)
 
         if wait_for:
             page.wait_for_selector(wait_for, timeout=TIMEOUT)
